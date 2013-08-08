d7ac66d46e118f2059fb51f105cae017fa43482a
Ryan Bosinger <rbosinger@gmail.copm>
2013-08-07 22:15:27 -0600

    Changed socket address.

--- public/client.js
+++ public/client.js
@@ -1,5 +1,5 @@
 // connect to our socket.io instance
-var socket = io.connect('http://127.0.0.1:3000');
+var socket = io.connect('http://ryanbosinger:3000');
 
 // keeps track of the last user-selected box
 var lastSelectedBox;
